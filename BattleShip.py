76f23aeddd6d9fdf4fca329f0cccfeadd757e1b4
Ruben kelly <ruben.kelly@outlook.com>
2021-01-14 11:00:20 +1300

    Screen initialisation works, needs some tweaking

--- BattleShip.py
+++ BattleShip.py
@@ -1,41 +1,53 @@
-import maths
+from tkinter import Tk, Label, Button
+from functools import partial
+import math
 
 class Board:
-    buttonsObjects = [[]]
-    buttonsClicked = [[]]
-    opponentButtonsObjects = [[]]
-    opponentButtonsClicked = [[]]
+    buttonsObjects = [[None]*10 for _ in range(10)]
+    buttonsClicked = [[None]*10 for _ in range(10)]
+    opponentButtonsObjects = [[None]*10 for _ in range(10)]
+    opponentButtonsClicked = [[None]*10 for _ in range(10)]
 
 
-    def __init__():
-        setupScreen()
-        opponentSetup()
+    def __init__(self, master):
+        for x in range(0,10):
+            for y in range(0,10):
+                self.buttonsClicked[x][y] = False
+        self.setupScreen()
+        self.opponentSetup()
+        print(self.buttonsClicked)
 
     #Sets up board with all correct variables
-    def setupScreen():
-        pass
+    def setupScreen(self):
+        for x in range(0,10):
+            for y in range(0,10):
+                self.buttonsObjects[x][y] = Button(root, command = partial(self.playMove,x,y))
+                self.buttonsObjects[x][y].config(textvariable = " ", width = 5, height = 3)
+                self.buttonsObjects[x][y].grid(row = x, column = y)
 
     #Takes move from player (via the buttons)
-    def playMove(x,y):
-        if checkMove(x,y):
-            buttonsClicked[x][y] = 1
+    def playMove(self,x,y):
+        #if self.checkMove(x,y):
+        self.buttonsClicked[x][y] = True
+        print(self.buttonsClicked)
 
     #Returns validity of a move
-    def checkMove(x,y):
-        pass
+    def checkMove(self,x,y):
+        return True
 
     #Sets up their board by random
     #(making sure no ships overlap)
-    def opponentSetup():
+    def opponentSetup(self):
         pass
 
     #Does a random move for the opponent
-    def opponentMove():
+    def opponentMove(self):
         pass
 
     #Returns state of the game
-    def isComplete():
+    def isComplete(self):
         pass
 
-
-gameBoard = Board()
+root = Tk()
+gameBoard = Board(root)
+root.mainloop()
